b15696e9b0f4829f9bb5ac21160733da42742df1
kdubaguntaepam <109850024+kdubaguntaepam@users.noreply.github.com>
2025-02-10 20:01:13 +0530

    Update API/Tests/UnitTests/ApiClientTests.cs

--- API/Tests/UnitTests/ApiClientTests.cs
+++ API/Tests/UnitTests/ApiClientTests.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace AutomationFramwork.API.Tests.UnitTests
 {
-    internal class ApiClientTests
+    public class ApiClientTests
     {
+        [Fact]
+        public async Task GetAsync_ShouldReturnSuccessStatusCode()
+        {
+            // Arrange
+            var httpClient = new HttpClient();
+            var apiClient = new BaseApiClient(httpClient);
+
+            // Act
+            var response = await apiClient.GetAsync("https://api.example.com/resource");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task PostAsync_ShouldReturnSuccessStatusCode()
+        {
+            // Arrange
+            var httpClient = new HttpClient();
+            var apiClient = new BaseApiClient(httpClient);
+            var content = new StringContent("{\"name\":\"test\"}", Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await apiClient.PostAsync("https://api.example.com/resource", content);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
